8166d98cf45b91976460810278c1c15fff7afa8e
onekil1 <onekil1xd194@gmail.com>
2025-06-13 00:20:38 +0300

    update reg and login func

--- prog/login.py
+++ prog/login.py
@@ -1,11 +1,54 @@
 import streamlit as st
-from streamlit.runtime.credentials import check_credentials
-
-import auth_func
+import sqlite3
+import hashlib
+import os
 
 for key in ["reg_bar", "log_bar", "info_user"]:
     if key not in st.session_state:
         st.session_state[key] = False
+
+def hash_password(password, salt=None):
+    if salt is None:
+        salt = os.urandom(32)
+    _key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt,100000)
+    return salt + _key
+
+def verify_password(stored_password, provided_password):
+    salt = stored_password[:32]
+    stored_key = stored_password[32:]
+    new_key = hashlib.pbkdf2_hmac('sha256', provided_password.encode('utf-8'), salt, 100000)
+    return new_key == stored_key
+
+def reg_user(work_name, full_name, login, password, password_check):
+    if password_check != password:
+        return "Указанные пароли не совпадают"
+    if len(password) <= 8:
+        return "Длинна пароля должна быть больше 8"
+    hashed_password = hash_password(password)
+    connection = sqlite3.connect(r"db/log=pass.db")
+    cursor = connection.cursor()
+    try:
+        cursor.execute('INSERT INTO auth (work_name, full_name, login, password) VALUES (?,?,?,?)', (work_name, full_name, login, hashed_password))
+        connection.commit()
+        return "Регистрация прошла успешно!"
+    except sqlite3.IntegrityError:
+        connection.rollback()
+        return "Учетная запись с таким именем уже существует!"
+    finally:
+        connection.close()
+
+def auth_user(login, password):
+    connection = sqlite3.connect(r"db/log=pass.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM auth WHERE login = ?', (login,))
+    result_search = cursor.fetchone()
+    if result_search is None:
+        return "Учетной записи с таким именем учетной записи не существует"
+    stored_password = result_search[4]
+    if not verify_password(stored_password, password):
+        return "Неверный логин или пароль"
+    else:
+        return result_search
 
 def log_button():
     with st.sidebar.form("Log_form", clear_on_submit=True):
@@ -17,7 +60,7 @@ def log_button():
         if not all([login, password]):
             return st.error("Введите свой логин и пароль")
         else:
-            log_inst = auth_func.auth_user(login, password)
+            log_inst = auth_user(login, password)
         if log_inst:
             st.session_state.info_user = log_inst
             return True
@@ -38,12 +81,11 @@ def reg_button():
         if not all([work_name, full_name, login, password]):
             return st.error("Все поля должны быть заполнены")
         else:
-            auth_inst = auth_func.reg_user(work_name, full_name, login, password, password_check)
+            auth_inst = reg_user(work_name, full_name, login, password, password_check)
             if auth_inst:
                 return st.success(auth_inst)
             else:
                 return st.error(auth_inst)
-
 
 col1, col2 = st.sidebar.columns(2)
 with col1:
@@ -60,3 +102,6 @@ if st.session_state.info_user is False:
         reg_button()
     if st.session_state.log_bar:
         log_button()
+
+if st.session_state.info_user:
+    st.switch_page("main_page.py")
