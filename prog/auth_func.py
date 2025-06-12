2275000bed2655e02b470c8023ed9c9ac0ef775b
onekil1 <onekil1xd194@gmail.com>
2025-06-12 17:12:27 +0300

    full working reg, added password lenght check

--- prog/auth_func.py
+++ prog/auth_func.py
@@ -17,18 +17,24 @@ def create_table():
     connection.commit()
     return True, "Таблица создана"
 
-def reg_user(work_name, full_name, login, password):
+def reg_user(work_name, full_name, login, password, password_check):
+
+    if password_check != password:
+        return "Указанные пароли не совпадают"
+    if len(password) <= 8:
+        return "Длинна пароля должна быть больше 8"
+
     connection = sqlite3.connect(r"C:\Users\onekil1\Coding\git_project\db\log=pass.db")
     cursor = connection.cursor()
     try:
         cursor.execute('INSERT INTO auth (work_name, full_name, login, password) VALUES (?,?,?,?)', (work_name, full_name, login, password))
         connection.commit()
-        return True, "Регистрация прошла успешно!"
+        return "Регистрация прошла успешно!"
     except sqlite3.IntegrityError:
         connection.rollback()
-        return False, "Такой login уже используется"
+        return "Учетная запись с таким именем уже существует!"
     finally:
         connection.close()
 
 if __name__ == "__main__":
-    print(reg_user("ЦМБП", "Басов Р.С.", "admin", "adminadmin"))
+    print(reg_user("ЦМБП", "Басов Р.С.", "admin", "adminadmin", "adminadmin"))
--- prog/interface.py
+++ prog/interface.py
@@ -2,15 +2,14 @@ import streamlit as st
 
 import auth_func
 
-for key in ["reg_bar"]:
+for key in ["reg_bar", "log_bar"]:
     if key not in st.session_state:
         st.session_state[key] = False
-
-if st.sidebar.button("Регистрация"):
-    st.session_state.reg_bar = not st.session_state.reg_bar
-
-if st.session_state.reg_bar:
-    with st.sidebar.form("reg_form"):
+def log_button():
+    with st.sidebar.form("Log_form", clear_on_submit=True):
+        st.subheader("Форма входа в систему")
+def reg_button():
+    with st.sidebar.form("reg_form", clear_on_submit=True):
         st.subheader("Форма регистрации")
         work_name = st.text_input("Введите название подразделения", placeholder="ЦМБП")
         full_name = st.text_input("Введите свое ФИО", placeholder="Иванов Иван Иванович")
@@ -18,16 +17,28 @@ if st.session_state.reg_bar:
         password = st.text_input("Введите пароль", type="password")
         password_check = st.text_input("Повторите пароль", type="password")
         submit = st.form_submit_button("Подтвердить регистрацию")
-        if submit:
-            if password_check != password:
-                st.error("Указанные пароли не совпадают")
-            elif len(password) < 8:
-                st.error("Длинна пароля должна быть < 8")
-            elif not all([work_name, full_name, login, password]):
-                st.error("Все поля должны быть заполнены")
+    if submit:
+        if not all([work_name, full_name, login, password]):
+            return st.error("Все поля должны быть заполнены")
+        else:
+            auth_inst = auth_func.reg_user(work_name, full_name, login, password, password_check)
+            if auth_inst:
+                return st.success(auth_inst)
             else:
-                auth_inst = auth_func.reg_user(work_name, full_name, login, password)
-                if auth_inst:
-                    st.error("Регистрация прошла успешно")
-                else:
-                    st.error("Ошибка при регистрации")
+                return st.error(auth_inst)
+
+
+col1, col2 = st.sidebar.columns(2)
+with col1:
+    if st.button("Регистрация"):
+        st.session_state.reg_bar = not st.session_state.reg_bar
+        st.session_state.log_bar = False
+with col2:
+    if st.button("Вход в систему"):
+        st.session_state.log_bar = not st.session_state.log_bar
+        st.session_state.reg_bar = False
+
+if st.session_state.reg_bar:
+    reg_button()
+if st.session_state.log_bar:
+    log_button()
