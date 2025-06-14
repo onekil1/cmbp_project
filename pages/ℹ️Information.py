3bd6be1f2789146ee49c8624539557c251d7ec52
onekil1 <onekil1xd194@gmail.com>
2025-06-14 22:30:10 +0300

    renamed

--- pages/ℹ️Information.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Информация", layout="wide")
-if st.session_state.info_user:
-    user_info = st.session_state.info_user[1]
-    st.sidebar.success(f"{user_info[2]}, Добро пожаловать!")
-else:
-    st.sidebar.error(f"Необходимо авторизироваться")
